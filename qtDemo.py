41c0ffc5318a202f29c258cd94cfad1bf460070e
shidaiguanghui <569393975@qq.com>
2025-07-23 17:00:15 +0800

    my modify

--- qtDemo.py
+++ qtDemo.py
@@ -2,4 +2,14 @@
 
 12
 
-5600test1178
+5600t2e2s2t1178
+
+def add_two_numbers(a, b, c=0):
+    """这个函数接收两个参数并返回它们的和"""
+    return a + b + c
+
+if __name__ == "__main__":
+    # 测试第一个函数
+    num1 = 5
+    num2 = 7
+    print(f"{num1} + {num2} = {add_two_numbers(num1, num2)}")
